a767001bdde28daac6f6affe02972e42c89f0180
ayush--1 <ayush5april@gmail.com>
2023-02-22 23:16:57 +0530

    profile added

--- src/components/profile/profiledisplay/profiledisplay.jsx
+++ src/components/profile/profiledisplay/profiledisplay.jsx
@@ -4,52 +4,12 @@ import { Link } from "react-router-dom";
 import "./profiledisplay.css";
 import { supabase } from "../../../lib/client";
 function ProfileDisplay({ setOpinion, opinion, token }) {
-  // const [fetchError, setFetchError] = useState(null);
-  // const [link, setLink] = useState("");
-  // const [image, setImage] = useState("");
-  // const fetchdetails = async () => {
-  //   e.preventDefault();
-  //   const { data, error } = await supabase
-  //     .from("links")
-  //     .select("twitter") //`content,images, users (raw_user_meta_data)`
-  //     .eq("user_name", token.session.user.user_metadata.name);
-  //   if (error) {
-  //     setFetchError("could not fetch the opnion");
-  //     setLink(null);
-  //     console.log(error);
-  //   }
-  //   if (data) {
-  //     setLink(data);
-  //     setFetchError(null);
-  //   }
-  // };
-  // const fetchImage = async () => {
-  //   const { data, error } = await supabase
-  //     .from("pfp")
-  //     .select("photo") //`content,images, users (raw_user_meta_data)`
-  //     .order("id");
-  //   if (error) {
-  //     setFetchError("could not fetch the opnion");
-  //     setImage(null);
-  //     console.log(error);
-  //   }
-  //   if (data) {
-  //     setImage(data);
-  //     setFetchError(null);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchdetails();
-  //   fetchImage();
-  // }, []);
-
-  console.log(token.session.user.user_metadata.image.publicUrl);
+  console.log(token.session.user.user_metadata.image);
   return (
     <div className="profile-details">
       <div className="yes">
         <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR00Z3totggyaGDo_l-7_-xlIBjXygktXx3g&usqp=CAU"
+          src={token.session.user.user_metadata.image}
           alt="imgs"
           className="profile-image"
         />
@@ -62,7 +22,7 @@ function ProfileDisplay({ setOpinion, opinion, token }) {
         <h2 className="profile-email">{token.session.user.email}</h2>
 
         <div className="idkkk">
-          <a href={token.session.user.user_metadata.link}>Link</a>
+          <a href={token.session.user.user_metadata.link}>twitter</a>
         </div>
       </div>
     </div>
--- src/components/profileUpdate/profileUpdate.jsx
+++ src/components/profileUpdate/profileUpdate.jsx
@@ -9,9 +9,9 @@ function ProfileUpdate({ token, url }) {
   const [avatarUrl, setAvatarUrl] = useState("");
   const [uploading, setUploading] = useState(false);
 
-  useEffect(() => {
-    if (avatarUrl) downloadImage(avatarUrl);
-  }, [avatarUrl]);
+  // useEffect(() => {
+  //   if (avatarUrl) downloadImage(avatarUrl);
+  // }, [avatarUrl]);
 
   const uploadimage = async (e) => {
     try {
@@ -23,36 +23,35 @@ function ProfileUpdate({ token, url }) {
       const fileExt = file.name.split(".").pop();
       const fileName = `${Math.random()}.${fileExt}`;
       const filePath = `${fileName}`;
-      let { error: uploadError } = await supabase.storage
+
+      let { data, error: uploadError } = await supabase.storage
         .from("posts")
-        .upload(filePath, file);
+        .upload(filePath, file, {
+          cacheControl: "3600",
+          upsert: false,
+        });
 
       if (uploadError) {
         throw uploadError;
       }
+      downloadImage(data);
+      console.log("data", data);
     } catch (error) {
       alert(error.message);
     } finally {
       setUploading(false);
     }
-    // downloadImage();
   };
 
-  const downloadImage = async (path) => {
-    try {
-      const { data, error } = await supabase.storage
-        .from("posts")
-        .getPublicUrl(path);
-      if (error) {
-        throw error;
-      }
-
-      const url = URL.createObjectURL(data);
-      setAvatarUrl(url);
-      console.log(url);
-    } catch (error) {
-      console.log("Error downloading image: ", error.message);
+  const downloadImage = async (media) => {
+    const { data, error } = supabase.storage
+      .from("posts")
+      .getPublicUrl(media.path);
+    if (error) {
+      throw error;
     }
+    console.log(data);
+    setAvatarUrl(data.publicUrl);
   };
 
   /////////////////////////////////////////////////////////////////////
@@ -118,54 +117,58 @@ function ProfileUpdate({ token, url }) {
 
   return (
     <div className="profile-update">
-      <div className="image-update">
-        {/* <form action=""></form> */}
-        <input
-          type="file"
-          id="single"
-          accept="image/*"
-          // value={image}
-          onChange={uploadimage}
-          disabled={uploading}
-        />
-
-        <button onClick={sendImageDetails}>update</button>
-      </div>
-
-      <div className="link_update">
-        <form action="" onSubmit={sendDetails}>
+      <div className="all-update">
+        <div className="image-update">
+          {/* <form action=""></form> */}
           <input
-            type="text"
-            placeholder="social link"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
+            type="file"
+            id="single"
+            accept="image/*"
+            // value={image}
+            onChange={uploadimage}
+            disabled={uploading}
           />
-          <button>update</button>
-        </form>
-      </div>
 
-      <div className="email-update">
-        <form action="" onSubmit={updateEmail}>
-          <input
-            type="email"
-            placeholder="update email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button>update</button>
-        </form>
-      </div>
+          <button onClick={sendImageDetails} className="update-button">
+            update
+          </button>
+        </div>
 
-      <div className="name-update">
-        <form action="/profile" onSubmit={updateName}>
-          <input
-            type="text"
-            placeholder="update name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button>update</button>
-        </form>
+        <div className="link-email-name-update">
+          <form action="" onSubmit={sendDetails}>
+            <input
+              type="text"
+              placeholder="social link"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button className="update-button">update</button>
+          </form>
+        </div>
+
+        <div className="link-email-name-update">
+          <form action="" onSubmit={updateEmail}>
+            <input
+              type="email"
+              placeholder="update email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button className="update-button">update</button>
+          </form>
+        </div>
+
+        <div className="link-email-name-update">
+          <form action="/profile" onSubmit={updateName}>
+            <input
+              type="text"
+              placeholder="update name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button className="update-button">update</button>
+          </form>
+        </div>
       </div>
     </div>
   );
